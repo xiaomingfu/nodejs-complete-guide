dd7f8cd89d6e333723714616bd0f678380a17743
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-20 23:59:18 -0800

    add redirect for create-user post

--- sec03-basic/assignment01/routes.js
+++ sec03-basic/assignment01/routes.js
@@ -1,18 +1,18 @@
-const fs = require("fs");
-
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
 
   if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write(
-      "<head><title>register page</title><body><form action='/create-user' method='POST'><input name='username'/><button type='submit'>submit</button></form></body></head>"
+      "<head><title>register page</title><body><form action='/create-user' method='POST'><input type='text' name='username'/><button type='submit'>submit</button></form></body></head>"
     );
     res.write("</html>");
     return res.end();
   }
   if (url === "/users") {
+    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write(
       "<head><title>user list</title><body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body></head>"
@@ -26,16 +26,20 @@ const requestHandler = (req, res) => {
     req.on("data", chunk => {
       body.push(chunk);
     });
-    return req.on("end", () => {
+    req.on("end", () => {
       const parseBody = Buffer.concat(body).toString();
-      console.log(parseBody);
+      const message = parseBody.split("=")[1];
+      console.log(message);
     });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   }
 
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write(
-    "<head><title>node basic concept assignment</title><body><h1>Hi, This is from server side</h1></body></head>"
+    "<head><title>node basic concept assignment</title><body><h1>404</h1></body></head>"
   );
   res.write("</html>");
   res.end();
