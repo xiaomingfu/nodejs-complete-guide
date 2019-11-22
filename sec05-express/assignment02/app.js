5506e96ab23c5d78c56373b77a72ecfcbdf6477a
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-21 16:29:41 -0800

    add two middleware with next()

--- sec05-express/assignment02/app.js
+++ sec05-express/assignment02/app.js
@@ -2,12 +2,21 @@ const express = require("express");
 
 const app = express();
 
+// app.use((req, res, next) => {
+//   console.log("This is one middleware");
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.log("This is another middleware");
+// });
+
 app.use("/users", (req, res, next) => {
-  console.log("This is user page");
+  console.log("This is user middleware");
+  res.send("<p>This middleware just handle /users</p>");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("This is home page");
+  console.log("This is / middleware");
   res.send("<h1>Welcome to my page</h1>");
 });
 app.listen(3000);
