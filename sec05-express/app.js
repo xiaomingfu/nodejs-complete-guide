f3ee4f502cc089007e1ae276394d6546e25b74f2
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-21 23:37:41 -0800

    add 404 page and refactor get and post method

--- sec05-express/app.js
+++ sec05-express/app.js
@@ -18,7 +18,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // server.listen(3000);
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
 app.listen(3000);
