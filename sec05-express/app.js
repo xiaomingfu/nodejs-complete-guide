fc47575d91c25ba0fe679d9af53139b6b14910ff
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-22 23:21:36 -0800

    add 404 html to app.js

--- sec05-express/app.js
+++ sec05-express/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const bodyParser = require("body-parser");
 
@@ -21,6 +22,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 app.listen(3000);
