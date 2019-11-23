71fcfc83d2aa7910c0ae262237725d54ec364b57
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-22 23:17:39 -0800

    add html file to routes

--- sec05-express/routes/shop.js
+++ sec05-express/routes/shop.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("this is another middleware");
-  res.send("<h1>Hello from Express!</h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
