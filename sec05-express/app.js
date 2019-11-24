4c70ba38a6e25dc3b915f0e7e553a5bf2caa258c
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-23 22:12:05 -0800

    add public dir for css files and add static path

--- sec05-express/app.js
+++ sec05-express/app.js
@@ -19,6 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // server.listen(3000);
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
