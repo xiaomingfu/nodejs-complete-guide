0577ffbaba15cce2aa873354b844529c6ce9519e
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-24 21:36:55 -0800

    add pug html template

--- project/routes/admin.js
+++ project/routes/admin.js
@@ -1,18 +1,22 @@
 const express = require("express");
+
 const path = require("path");
 
 const rootDir = require("../util/path");
+
 const router = express.Router();
 
+const products = [];
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { props: products, pageTitle: "Add Product" });
 });
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
