cf00bcf55fd8caa219323fefe09d81d461551f0e
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-24 20:10:44 -0800

    reorganize repo

--- assignment03/index.js
+++ assignment03/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 
 const path = require("path");
-const route = express.Router();
 
 const mainRoutes = require("./routes/main");
 const adminRoutes = require("./routes/admin");
+
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(mainRoutes);
 app.use(adminRoutes);
 
