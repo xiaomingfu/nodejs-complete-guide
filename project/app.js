12c3a313a0ecd2f6636e135ecc143ad2922c5a99
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-24 22:39:26 -0800

    add dynamic title

--- project/app.js
+++ project/app.js
@@ -27,6 +27,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { pageTitle: "page not found" });
 });
 app.listen(3000);
--- project/routes/admin.js
+++ project/routes/admin.js
@@ -9,7 +9,11 @@ const router = express.Router();
 const products = [];
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.render("add-product", { props: products, pageTitle: "Add Product" });
+  res.render("add-product", {
+    props: products,
+    pageTitle: "Add Product",
+    path: "/admin/add-product"
+  });
 });
 
 // /admin/add-product => POST
