65cf89706895dc1a982a666e3d0f139232e8cc13
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-25 16:20:33 -0800

    wrap up end with ejs

--- project/app.js
+++ project/app.js
@@ -2,38 +2,16 @@ const express = require("express");
 const path = require("path");
 
 const bodyParser = require("body-parser");
-// const expressHbs = require("express-handlebars");
 
 const app = express();
 
-// app.engine(
-//   "hbs",
-//   expressHbs({
-//     layoutsDir: "views/layouts/",
-//     defaultLayout: "main-layout",
-//     extname: "hbs"
-//   })
-// );
-
-// app.set("view engine", "hbs");
-// app.set("view engine", "pug");
-// app.set("views", "views");
-
 app.set("view engine", "ejs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use("/", (req, res, next) => {
-//   console.log("This is always run");
-//   next();
-// });
-
-// const server = http.createServer(app);
-
-// server.listen(3000);
 
 app.use(express.static(path.join(__dirname, "public")));
 
