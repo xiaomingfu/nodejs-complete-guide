b6d8cfbedda49f3d46f54587b3d167d11974b73a
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-27 02:27:21 -0800

    store data in file

--- project/models/product.js
+++ project/models/product.js
@@ -1,16 +1,41 @@
 const path = require("path");
 const fs = require("fs");
 
+let products = [];
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
     return products;
   }
 };
