e7da24de2e08a4439180b1d08d90af463ea12a14
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-27 22:05:29 -0800

    add pages

--- project/controllers/products.js
+++ project/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 const getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product"
   });
@@ -14,7 +14,7 @@ const postAddProduct = (req, res, next) => {
 };
 const getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       prods: products,
       pageTitle: "Shop",
       path: "/",
