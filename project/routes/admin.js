3cd492d9088aabc3b964a251ea67cc5489c08e7f
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-28 16:23:51 -0800

    register routes

--- project/routes/admin.js
+++ project/routes/admin.js
@@ -6,7 +6,15 @@ const productController = require("../controllers/products.js");
 // /admin/add-product => GET
 router.get("/add-product", productController.getAddProduct);
 
+// /admin/product => GET
+router.get("/products", (req, res, next) => {
+  res.render("admin/products");
+});
+
 // /admin/add-product => POST
 router.post("/add-product", productController.postAddProduct);
 
+router.get("/product-detail", (req, res, next) => {
+  res.render("shop/product-list");
+});
 module.exports = router;
--- project/routes/shop.js
+++ project/routes/shop.js
@@ -6,4 +6,16 @@ const router = express.Router();
 
 router.get("/", productsController.getProducts);
 
+router.get("/cart", (req, res, next) => {
+  res.render("shop/cart.ejs");
+});
+
+router.get("/products", (req, res, next) => {
+  res.render("shop/product-list.ejs");
+});
+
+router.get("/checkout", (req, res, next) => {
+  res.render("shop/checkout.ejs");
+});
+
 module.exports = router;
