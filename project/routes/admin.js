a48d66b146b37cc39251c530552393feacaa96e6
Xiaoming Fu <xmingfu86@gmail.com>
2019-11-28 22:50:13 -0800

    fix admin route bug

--- project/routes/admin.js
+++ project/routes/admin.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 const adminController = require("../controllers/admin.js");
 // /admin/add-product => GET
-router.get("/admin/add-product", adminController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
 
 // /admin/product => GET
-router.get("/admin/products", adminController.getProducts);
+router.get("/products", adminController.getProducts);
 
 // /admin/add-product => POST
-router.post("/admin/add-product", adminController.postAddProduct);
+router.post("/add-product", adminController.postAddProduct);
 
 module.exports = router;
