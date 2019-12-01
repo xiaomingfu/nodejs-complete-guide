b525be1c16fa55e667207cad598b2d26f27aab1b
Xiaoming Fu <xmingfu86@gmail.com>
2019-12-01 00:01:36 -0800

    add cart model

---
+++ project/models/cart.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+
+module.exports = class Cart {
+  //   constructor() {
+  //     this.products = [];
+  //     this.totalPrice = 0;
+  //   }
+
+  static addProduct(id, productPrice) {
+    //Fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      //Analyze the cart => Fine existing product
+      const existingProductIndex = cart.products.findIndex(
+        product => product.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updateProduct;
+      if (existingProduct) {
+        updateProduct = { ...existingProduct };
+        updateProduct.qty = updateProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updateProduct;
+      } else {
+        updateProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updateProduct];
+      }
+      cart.totlalPrice = cart.totalPrice + productPrice;
+      fs.writeFile(p, JSON.stringify(cart), err => {
+        console.log(err);
+      });
+    });
+  }
+};
