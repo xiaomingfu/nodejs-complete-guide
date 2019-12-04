45b0b0cf6f3192cb3df7dbcddbec8e2c2c604cb8
Xiaoming Fu <xmingfu86@gmail.com>
2019-12-03 22:51:05 -0800

    update post edit request

--- project/controllers/admin.js
+++ project/controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
@@ -35,6 +35,23 @@ exports.getEditProduct = (req, res, next) => {
       product: product
     });
   });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updateTitle = req.body.title;
+  const updatePrice = req.body.price;
+  const updateImageUrl = req.body.imageUrl;
+  const updateDesc = req.body.description;
+  const updateProduct = new Product(
+    prodId,
+    updateTitle,
+    updateImageUrl,
+    updateDesc,
+    updatePrice
+  );
+  updateProduct.save();
+  res.redirect("/admin/products");
 };
 
 exports.getProducts = (req, res, next) => {
