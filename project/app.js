7c6159a9d5318980efcfdb94e4b84462f1ffe985
Xiaoming Fu <xmingfu86@gmail.com>
2019-12-08 21:34:42 -0800

    use sequelize to add data to MySql

--- project/app.js
+++ project/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -32,4 +32,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then(result => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
