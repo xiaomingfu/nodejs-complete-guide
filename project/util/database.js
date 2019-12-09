37b0b3aadc2069bb187ab2dce774ae3869c56157
Xiaoming Fu <xmingfu86@gmail.com>
2019-12-08 21:23:28 -0800

    use sequelize to add data

--- project/util/database.js
+++ project/util/database.js
@@ -1,10 +1,19 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "nodecomplete",
-  password: ""
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "nodecomplete",
+//   password: ""
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("nodecomplete", "root", "", {
+  dialect: "mysql",
+  host: "localhost"
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
