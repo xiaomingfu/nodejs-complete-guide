0cceff20bd9e4b7a6109645f86a2584abf0dc092
Xiaoming Fu <xmingfu86@gmail.com>
2019-12-15 02:40:08 -0800

    add mongodb database connect

--- project/util/database.js
+++ project/util/database.js
@@ -1,19 +1,15 @@
-// const mysql = require("mysql2");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "nodecomplete",
-//   password: ""
-// });
+const mongoConnect = callback => {
+  MongoClient.connect(
+    "mongodb+srv://xiaoming:111@cluster0-fvtw9.mongodb.net/test?retryWrites=true&w=majority"
+  )
+    .then(client => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch(err => console.log(err));
+};
 
-// module.exports = pool.promise();
-
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("nodecomplete", "root", "", {
-  dialect: "mysql",
-  host: "localhost"
-});
-
-module.exports = sequelize;
+module.exports = mongoConnect;
