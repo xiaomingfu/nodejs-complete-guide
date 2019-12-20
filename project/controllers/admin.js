34757bb4a2555a66863c6f797e2e0ea819877a04
Xiaoming Fu <xmingfu86@gmail.com>
2019-12-19 23:27:33 -0800

    refactor admin controller to make admin products work

--- project/controllers/admin.js
+++ project/controllers/admin.js
@@ -75,20 +75,19 @@ exports.postAddProduct = (req, res, next) => {
 //     });
 // };
 
-// exports.getProducts = (req, res, next) => {
-//   req.user
-//     .getProducts()
-//     .then(products => {
-//       res.render("admin/products", {
-//         prods: products,
-//         pageTitle: "Admin Products",
-//         path: "/admin/products"
-//       });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// };
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+    .then(products => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 // exports.postDeleteProduct = (req, res, next) => {
 //   const productId = req.body.productId;
--- project/routes/admin.js
+++ project/routes/admin.js
@@ -7,7 +7,7 @@ const adminController = require("../controllers/admin");
 router.get("/add-product", adminController.getAddProduct);
 
 // /admin/product => GET
-// router.get("/products", adminController.getProducts);
+router.get("/products", adminController.getProducts);
 
 // /admin/add-product => POST
 router.post("/add-product", adminController.postAddProduct);
