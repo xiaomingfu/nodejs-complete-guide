b0b9af4f96a2ce825b59df6f4befa40bc7a984dd
Xiaoming Fu <xmingfu86@gmail.com>
2019-12-22 23:55:10 -0800

    refactor user model with mongodb

--- project/models/user.js
+++ project/models/user.js
@@ -1,22 +1,44 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
-const sequelize = require("../util/database");
+const ObjectId = mongodb.ObjectId;
 
-const User = sequelize.define("user", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
+class User {
+  constructor(username, email, id) {
+    this.name = username;
+    this.email = email;
+    this._id = id ? new mongoDb.ObjectId(id) : null;
   }
-});
+
+  save() {
+    const db = getDb();
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection("users")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection("users").insertOne(this);
+    }
+    return dbOp
+      .then(result => {
+        console.log(result);
+        return result;
+      })
+      .catch(err => console.log(err));
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch(err => console.log(err));
+  }
+}
 
 module.exports = User;
