6181e6881f0fc3272e8996e0c140069dbb48b1de
Xiaoming Fu <xmingfu86@gmail.com>
2019-12-26 01:56:23 -0800

    refactor req.user

--- project/app.js
+++ project/app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findByPk(1)
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
