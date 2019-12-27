f23d7d4658fd26bf34a8547b9f4610795854d967
Xiaoming Fu <xmingfu86@gmail.com>
2019-12-27 14:43:08 -0800

    use mongoose connect

--- project/app.js
+++ project/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
+
 const User = require("./models/user");
 
 const app = express();
@@ -39,4 +40,13 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
-mongoConnect(() => app.listen(3000));
+mongoose
+  .connect(
+    "mongodb+srv://xiaoming:111@cluster0-fvtw9.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
