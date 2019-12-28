cbec556ebd7b68454ae4d08a3b4a97865292581c
Xiaoming Fu <xmingfu86@gmail.com>
2019-12-27 22:42:55 -0800

    refactor index and proudcts route with mongoose .find() method

--- project/routes/shop.js
+++ project/routes/shop.js
@@ -4,7 +4,7 @@ const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-// router.get("/", shopController.getIndex);
+router.get("/", shopController.getIndex);
 
 // router.get("/cart", shopController.getCart);
 
@@ -16,7 +16,7 @@ const router = express.Router();
 
 // router.post("/create-order", shopController.postOrder);
 
-// router.get("/products", shopController.getProducts);
+router.get("/products", shopController.getProducts);
 
 // router.get("/products/:productId", shopController.getProduct);
 
