7d4acf970ff0c020812b17640358f1f3ae3aa178
Xiaoming Fu <xmingfu86@gmail.com>
2019-12-29 23:15:39 -0800

    refactor addToCart in user model

--- project/models/user.js
+++ project/models/user.js
@@ -25,6 +25,30 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.methods.addToCart = function(product) {
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString();
+  });
+
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
 
 // const mongodb = require("mongodb");
@@ -58,7 +82,7 @@ module.exports = mongoose.model("User", userSchema);
 //     let newQuantity = 1;
 //     const updatedCartItems = [...this.cart.items];
 
-//     if (cartProductItem >= 0) {
+//     if (cartProductIndex >= 0) {
 //       newQuantity = this.cart.items[cartProductIndex].quantity + 1;
 //       updatedCartItems[cartProductIndex].quantity = newQuantity;
 //     } else {
