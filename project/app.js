e34fc71c11dac519a5ec01d3662610bf0392f94e
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-04 01:16:10 -0800

    use csrf() in application

--- project/app.js
+++ project/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -16,6 +17,8 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: "sessions"
 });
+
+const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -43,6 +46,8 @@ app.use(
     store: store
   })
 );
+
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
