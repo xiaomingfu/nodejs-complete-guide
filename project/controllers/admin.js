c0689c459d20b487814f1eabf7d5808fe24b7790
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-07 17:01:21 -0800

    check userId for edit and delete product

--- project/controllers/admin.js
+++ project/controllers/admin.js
@@ -64,23 +64,26 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then(product => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect("/");
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.ImageUrl = updatedImageUrl;
       product.description = updatedDesc;
-      return product.save();
+      return product.save().then(result => {
+        console.log("Update data");
+        res.redirect("/admin/products");
+      });
     })
-    .then(result => {
-      console.log("Update data");
-      res.redirect("/admin/products");
-    })
+
     .catch(err => {
       console.log(err);
     });
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({ userId: req.user._id })
     // .select("title price -_id")
     // .populate("userId", "name")
     .then(products => {
@@ -98,7 +101,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  Product.deleteOne({ _id: prodId }, { userId: req.user._id })
     .then(() => {
       console.log("Destroyed");
       res.redirect("/admin/products");
