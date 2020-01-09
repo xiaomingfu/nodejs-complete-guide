0aac81c0a4daa37d57e862cdb2c99a22b480ef91
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-09 15:46:08 -0800

    add validation to login

--- project/controllers/auth.js
+++ project/controllers/auth.js
@@ -30,7 +30,9 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "login",
-    errorMessage: message
+    errorMessage: message,
+    oldInput: { email: "", password: "" },
+    validationErrors: []
   });
 };
 
@@ -38,19 +40,26 @@ exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
-    return res.status(422).render("ayth/login", {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/login", {
       path: "/login",
       pageTitle: "Login",
-      errorMessage: errors.array[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: { email: email, password: password },
+      validationErrors: errors.array()
     });
   }
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
-        req.flash("error", "Invalid email or password.");
-        return res.redirect("/login");
+        return res.status(422).render("auth/login", {
+          path: "/login",
+          pageTitle: "Login",
+          errorMessage: "Invalid email or password",
+          oldInput: { email: email, passwor: password },
+          validationErrors: [{ param: "email" }]
+        });
       }
       bcrypt
         .compare(password, user.password)
@@ -63,8 +72,13 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
-          req.flash("error", "Invalid email or password.");
-          res.redirect("/login");
+          return res.status(422).render("auth/login", {
+            path: "/login",
+            pageTitle: "Login",
+            errorMessage: "Invalid password",
+            oldInput: { email: email, password: password },
+            validationErrors: [{ param: "password" }]
+          });
         })
         .catch(err => {
           console.log(err);
