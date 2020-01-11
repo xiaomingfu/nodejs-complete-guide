4dfb9aaefa29abd5a92ab819d87511aff30ccc2d
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-10 22:39:48 -0800

    configure multer storage data

--- project/app.js
+++ project/app.js
@@ -22,6 +22,14 @@ const store = new MongoDBStore({
 
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  }
+});
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -39,7 +47,7 @@ const authRoutes = require("./routes/auth");
 //   });
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: "images" }).single("image"));
+app.use(multer({ storage: fileStorage }).single("image"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
