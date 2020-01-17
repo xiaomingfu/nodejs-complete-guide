80fa1e195f2d3d29bc4ea88213425574d70c989e
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-17 15:57:31 -0800

    delete product on client side

--- project/controllers/admin.js
+++ project/controllers/admin.js
@@ -191,22 +191,21 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findById(prodId).then(product => {
-    if(!product){
-      return next(new Error("Product not found."))
-    }
-    fileHelper.deleteFile(product.imageUrl);
-    return Product.deleteOne({_id:prodId, userId:req.user._id});
-  })
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then(product => {
+      if (!product) {
+        return next(new Error("Product not found."));
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
     .then(() => {
       console.log("Destroyed");
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "Success!" });
     })
     .catch(err => {
-      const error = new Error(err);
-      error.htttpStatus.Code = 500;
-      return next(error);
+      res.status(500).json({ message: "Delete product failed!" });
     });
 };
