97131163181685bc02c6e60562f060c9e9328f7d
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-20 00:13:46 -0800

    send json data through req and res

--- project2/app.js
+++ project2/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const feedRoutes = require("./routes/feed");
 
 const app = express();
 
-const feedRoutes = require("./routes/feed");
+// app.use(bodyParser.urlencoded()); //x-www-form-urlencoded
+
+app.use(bodyParser.json()); //application/json
 
 app.use("/feed", feedRoutes);
 
--- project2/routes/feed.js
+++ project2/routes/feed.js
@@ -4,7 +4,10 @@ const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
-//  get /feed/post
+//  get /feed/posts
 router.get("/posts", feedController.getPosts);
+
+//Post /feed/post
+router.post("/post", feedController.createPost);
 
 module.exports = router;
