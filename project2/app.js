2f062a3398e9aa62ff8988598c51e2d0d5bb98fb
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-20 01:01:16 -0800

    add middleware to avoid Cross-Origin-Resouse-Sharing

--- project2/app.js
+++ project2/app.js
@@ -9,6 +9,15 @@ const app = express();
 
 app.use(bodyParser.json()); //application/json
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8000, () => {
