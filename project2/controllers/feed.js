4a0b6ee7f7b9b2a46084ec3d607501d961712272
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-21 00:57:02 -0800

    add validation on post page

--- project2/controllers/feed.js
+++ project2/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -15,11 +17,26 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: "Validation failed, entered data is incorrect.",
+        errors: errors.array()
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
   //Create post in db
   res.status(201).json({
     message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title: title, content: content }
+    post: {
+      id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Ming" },
+      createdAt: new Date()
+    }
   });
 };
--- project2/routes/feed.js
+++ project2/routes/feed.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const { body } = require("express-validator/check");
 const feedController = require("../controllers/feed");
 
 const router = express.Router();
@@ -8,6 +8,17 @@ const router = express.Router();
 router.get("/posts", feedController.getPosts);
 
 //Post /feed/post
-router.post("/post", feedController.createPost);
+router.post(
+  "/post",
+  [
+    body("title")
+      .trim()
+      .isLength({ min: 5 }),
+    body("content")
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
 
 module.exports = router;
