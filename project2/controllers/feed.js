a303346d922766999fdd93717b799aa733d20a1c
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-21 15:16:28 -0800

    store model in database

--- project2/controllers/feed.js
+++ project2/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator/check");
 
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -19,24 +21,28 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: "Validation failed, entered data is incorrect.",
-        errors: errors.array()
-      });
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect.",
+      errors: errors.array()
+    });
   }
   const title = req.body.title;
   const content = req.body.content;
-  //Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: "Ming" },
-      createdAt: new Date()
-    }
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/prod1.jpg",
+    creator: { name: "Ming" }
   });
+  post
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
