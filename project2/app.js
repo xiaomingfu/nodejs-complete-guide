6722c76cb96ec736b6dcb6ba255c894159778d24
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-21 16:15:51 -0800

    refactor error handling and add static image path

--- project2/app.js
+++ project2/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
+
 const feedRoutes = require("./routes/feed");
 
 const app = express();
@@ -8,6 +10,8 @@ const app = express();
 // app.use(bodyParser.urlencoded()); //x-www-form-urlencoded
 
 app.use(bodyParser.json()); //application/json
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,6 +24,13 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongoose
   .connect(
