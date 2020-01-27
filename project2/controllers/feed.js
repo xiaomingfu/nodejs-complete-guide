cce563b0a65cbdece32671d037fb049d7be85031
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-27 11:04:41 -0800

    finish update edit post

--- project2/controllers/feed.js
+++ project2/controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const { validationResult } = require("express-validator/check");
 
 const Post = require("../models/post");
@@ -75,6 +78,12 @@ exports.getPost = (req, res, next) => {
 };
 
 exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect.");
+    error.statusCode = 422;
+    throw error;
+  }
   const postId = req.params.postId;
   const title = req.body.title;
   const content = req.body.content;
@@ -87,4 +96,33 @@ exports.updatePost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error("Cound not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then(result => {
+      res.status(200).json({ message: "Post updated!", post: result });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, err => console.log(err));
 };
