4ff5d4ad3354a03888c8450f41be472890d66c23
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-27 16:57:43 -0800

    add auth validation

--- project2/routes/auth.js
+++ project2/routes/auth.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator/check");
 
-router.put("/signup");
+const User = require("../models/user");
+const authController = require("../controllers/auth");
+
+router.put(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a vailid email.")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject("Email address already exists!");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password")
+      .trim()
+      .isLength({ min: 5 }),
+    body("name")
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  authController.signup
+);
 
 module.exports = router;
