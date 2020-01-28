77088e3a52e1725d5078ecae935f994090c9fd56
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-28 14:57:56 -0800

    add password crypt

--- project2/controllers/auth.js
+++ project2/controllers/auth.js
@@ -1,4 +1,5 @@
-const { validationResult } = require("express-validator/check");
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 
@@ -13,4 +14,23 @@ exports.signup = (req, res, next) => {
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPw => {
+      const user = new User({
+        email: email,
+        name: name,
+        password: hashedPw
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.status(200).json({ message: "User created.", userId: result._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
