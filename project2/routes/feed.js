741e5f467cb70e678dcefcef384d4d49f7abe7e4
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-29 00:32:05 -0800

    add isAuth as middleware

--- project2/routes/feed.js
+++ project2/routes/feed.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { body } = require("express-validator/check");
-const feedController = require("../controllers/feed");
 
+const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/isAuth");
 const router = express.Router();
 
 //  get /feed/posts
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 //Post /feed/post
 router.post(
