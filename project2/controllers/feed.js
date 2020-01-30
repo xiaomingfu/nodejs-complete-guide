a6c57c5bd7fee4ffe22208709a50ba66ee56b233
Xiaoming Fu <xmingfu86@gmail.com>
2020-01-29 22:36:26 -0800

    clear post-user relation

--- project2/controllers/feed.js
+++ project2/controllers/feed.js
@@ -168,7 +168,13 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndRemove(postId);
     })
     .then(result => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then(user => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then(result => {
       res.status(200).json({ message: "Post deleteded!" });
     })
     .catch(err => {
