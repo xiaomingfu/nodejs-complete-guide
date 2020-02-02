bf9ae0c451512551f2a5b1e17af9d897c4852b07
Xiaoming Fu <xmingfu86@gmail.com>
2020-02-01 18:22:22 -0800

    add socket for init

--- project2/app.js
+++ project2/app.js
@@ -63,7 +63,7 @@ mongoose
   )
   .then(result => {
     const server = app.listen(8080);
-    const io = require("socket.io")(server);
+    const io = require("./socket").init(server);
     io.on("connection", socket => {
       console.log("Client connection!");
     });
