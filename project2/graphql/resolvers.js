ea5e1efa1fcd1c00f59f191011eee922c2682961
Xiaoming Fu <xmingfu86@gmail.com>
2020-02-10 17:50:42 -0800

    add validator

--- project2/graphql/resolvers.js
+++ project2/graphql/resolvers.js
@@ -1,8 +1,24 @@
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
+
 const User = require("../models/user");
 
 module.exports = {
   createUser: async function({ userInput }, req) {
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "E-mail is invalid." });
+    }
+    if (
+      !validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: "Password too short!" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid input.");
+      throw error;
+    }
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
       const error = new Error("User exists already!");
@@ -11,7 +27,7 @@ module.exports = {
     const hashPwd = await bcrypt.hash(userInput.password, 12);
     const user = new User({
       email: userInput.email,
-      password: hasPwd,
+      password: hashPwd,
       name: userInput.name
     });
     const createUser = await user.save();
