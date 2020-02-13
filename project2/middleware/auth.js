956247b67d873601036bd8187d55b2cf2d8d138c
Xiaoming Fu <xmingfu86@gmail.com>
2020-02-12 23:14:11 -0800

    refactor isAuth for auth

--- project2/middleware/auth.js
+++ project2/middleware/auth.js
@@ -3,23 +3,22 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
-    const err = new Error("Not authorized.");
-    err.statusCode = 401;
-    throw err;
+    req.isAuth = false;
+    return next();
   }
   const token = authHeader.split(" ")[1];
   let decodedToken;
   try {
-    decodedToken = jwt.verify(token, "somesuperscret");
+    decodedToken = jwt.verify(token, "somesupersecret");
   } catch (err) {
-    err.statusCode = 500;
-    throw err;
+    req.isAuth = false;
+    return next();
   }
   if (!decodedToken) {
-    const error = new Error("Not authorized.");
-    error.statusCode = 401;
-    throw error;
+    req.isAuth = false;
+    return next();
   }
   req.userId = decodedToken.userId;
+  req.isAuth = true;
   next();
 };
