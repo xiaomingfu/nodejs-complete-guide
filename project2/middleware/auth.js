807e6fa44ef2e7a362c75aba90c277157644b649
Xiaoming Fu <xmingfu86@gmail.com>
2020-02-21 00:33:07 -0800

    add test for token authentication

--- project2/middleware/auth.js
+++ project2/middleware/auth.js
@@ -4,6 +4,8 @@ module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
     // req.isAuth = false;
+
+    // for test model
     const error = new Error("Not Authenticated.");
     error.statusCode = 401;
     throw error;
@@ -14,14 +16,21 @@ module.exports = (req, res, next) => {
   try {
     decodedToken = jwt.verify(token, "somesupersecret");
   } catch (err) {
-    req.isAuth = false;
-    return next();
+    // for test model
+    err.statusCode = 500;
+    throw err;
+    // req.isAuth = false;
+    // return next();
   }
   if (!decodedToken) {
-    req.isAuth = false;
-    return next();
+    // for test model
+    const error = new Error("Not authenticated.");
+    error.statusCode = 401;
+    throw error;
+    // req.isAuth = false;
+    // return next();
   }
   req.userId = decodedToken.userId;
-  req.isAuth = true;
+  // req.isAuth = true;
   next();
 };
--- project2/test/auth-middleware.js
+++ project2/test/auth-middleware.js
@@ -2,11 +2,24 @@ const expect = require("chai").expect;
 
 const authMiddleware = require("../middleware/auth");
 
-it("should throw an error if no authorization header is present", function() {
-  const req = {
-    get: function() {
-      return null;
-    }
-  };
-  expect(authMiddleware.bind(this,req, {}, () => {}).to.throw("Not authenticated.");
+describe("Auth middleware", function() {
+  it("should throw an error if no authorization header is present", function() {
+    const req = {
+      get: function() {
+        return null;
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not Authenticated."
+    );
+  });
+
+  it("should thorw an error if the authentication header is only one string.", function() {
+    const req = {
+      get: function(hearderName) {
+        return "xyz";
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
